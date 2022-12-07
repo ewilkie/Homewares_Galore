82434151aa8d6d2b203650cfcc689cc1dc4c3c3f
ewilkie <emilie.wilkie@gmail.com>
2022-12-07 16:00:13 +1100

    feddback tab

--- js/main.js
+++ js/main.js
@@ -1,46 +1,10 @@
-// Slideshow/ carousell javascript 
-/*
-var slideIndex = 1;
-showSlides(slideIndex);
-
-var timer;
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  if (timer) {
-    clearTimeout(timer);
-  }
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace("active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  timer = setTimeout( function(){ plusSlides(1); }, 3000 );
-}
-*/ 
-
 // about us button read more 
 const aboutReadMore = document.querySelector('.about_section-button');
 
 aboutReadMore.addEventListener("click", function(){
   document.querySelector('.moreabout').classList.toggle('moreabout-toggle');
 
- 
+  // so that text can return to original when div hidden/closed
   const initialText = 'READ MORE';
 
   if (aboutReadMore.textContent.includes(initialText)) {
@@ -51,15 +15,32 @@ aboutReadMore.addEventListener("click", function(){
 });
 
 
-/* for the feedback container
-const menuController = document.getElementById('feedback-container');
+// to open and close the feedback div
+const menuController = document.querySelector('.feedback-button');
 menuController.addEventListener('click', function() {
-  
-  // setAttribute way
-  // Warning: This over-writes previous classes. Use add/remove/toggle to impact just one class
-  //document.querySelector('.menu').setAttribute('class', 'menu menu-open');
 
-  // classList way
-  document.querySelector('.feedback-container').classList.toggle('menu-open');
+  // toggle feedback open and close
+  document.querySelector('.feedback-container').classList.toggle('feedback-open');
 });
-*/
+
+
+// feed back form error handling
+function handleFormSubmission(event) {
+  event.preventDefault();
+
+  const nameErrorContainer = document.querySelector('.name-error-container');
+  nameErrorContainer.innerText = '';
+  
+  const firstNameInputEl = document.querySelector('#lname');
+  
+  firstNameInputEl.style.borderColor = 'grey';
+  
+  if (firstNameInputEl.value.length <= 0) {
+    nameErrorContainer.innerText = 'This field cannot be empty';
+    firstNameInputEl.style.borderColor = 'red';
+  }
+}
+
+const applicationForm = document.querySelector('.feedback-form');
+applicationForm.addEventListener('submit', handleFormSubmission);
+
