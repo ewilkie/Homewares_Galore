861e05ec9e4d70defce8afc77b16dca8ea06646d
ewilkie <emilie.wilkie@gmail.com>
2022-12-08 13:10:09 +1100

    stores framework

---
+++ js/stores.js
@@ -0,0 +1,100 @@
+/**************************** */
+/* Javascript for stores page */
+/**************************** */
+const storeObj = [
+    {
+    name: "Ballarat",
+    address: "abs street",
+    tlf: "123456895",
+    hours: {
+      week: '09:00-17:30',
+      sat: '10:00-16:30',
+      sun: '10:00-12:30',
+      },
+    },
+    {
+      name: "Bendigo",
+      address: "Bendigo street",
+      tlf: "123456895",
+      hours: {
+      week: '09:00-17:30',
+      sat: '11:00-16:30',
+      sun: '11:00-12:30',
+    },
+    }
+   ]
+  
+  /* loop to populate page with information stored in array */
+  for (let i=0; i < storeObj.length; i++) {
+    
+    /* create div for each store */
+    const storeItem = document.createElement('div');
+    /* add styling class for div */
+    storeItem.classList.add('store-container-indiv');  
+   /* add div to page */
+    document.querySelector('.store-container').appendChild(storeItem);
+    
+    /* add title to page */
+    const title = document.createElement('h2');
+    title.classList.add('h2');
+    title.classList.add('store-title');
+    title.innerText = "Homewares Galore - " + storeObj[i].name ;
+    storeItem.appendChild(title);
+    
+    /* second indiv container */
+    const details = document.createElement('div');
+    details.classList.add('store-details');
+    storeItem.appendChild(details);
+    
+    /* select second item for appending inside div */ 
+    const sec = storeItem.querySelector('.store-details');
+    
+    /* append address to second div */
+    const addr = document.createElement('div');
+    addr.classList.add('address');
+    addr.innerText = storeObj[i].address;
+    sec.appendChild(addr);
+    
+    const tlf = document.createElement('div');
+    tlf.classList.add('tlf');
+    tlf.innerText = storeObj[i].tlf;
+    sec.appendChild(tlf);
+    
+    /* create a list for opening hours */ 
+    const hrs = document.createElement('ul');
+    sec.appendChild(hrs);
+    
+    let openingHours = storeObj[i].hours;
+    
+    console.log(openingHours);
+    for(var j in openingHours) {
+      console.log(j);
+      const oh = document.createElement('li');
+      oh.innerText = j + " - " + openingHours[j];
+      sec.appendChild(oh);
+    }
+    
+    
+    /* third div - grid area store-buttons */
+    const buttons = document.createElement('div');
+    buttons.classList.add('store-buttons');
+    storeItem.appendChild(buttons);  
+    const third = storeItem.querySelector('.store-buttons');
+    
+    /* store information button */
+    const storeInfo = document.createElement('div');
+    storeInfo.classList.add('store-info');
+    storeInfo.innerText= 'View store information';
+    third.appendChild(storeInfo);
+    
+    const storeDir = document.createElement('div');
+    storeDir.classList.add('store-info');
+    storeDir.classList.add('directions');
+    storeDir.innerText= 'Get directions';
+    third.appendChild(storeDir);
+    
+  }
+  
+  
+  
+  
