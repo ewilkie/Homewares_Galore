05fa3b7de7fc9b7a168dbf8bfd0da3580f728328
ewilkie <emilie.wilkie@gmail.com>
2022-12-08 15:57:45 +1100

    css

--- js/stores.js
+++ js/stores.js
@@ -3,7 +3,27 @@
 /**************************** */
 const storeObj = [
     {
+    name: "Melbourne CBD",
+    address: "abs street",
+    tlf: "123456895",
+    hours: {
+        week: '09:00-17:30',
+        sat: '10:00-16:30',
+        sun: '10:00-12:30',
+        },
+    },
+    {
     name: "Ballarat",
+    address: "abs street",
+    tlf: "123456895",
+    hours: {
+      week: '09:00-17:30',
+      sat: '10:00-16:30',
+      sun: '10:00-12:30',
+      },
+    },
+    {
+    name: "Melbourne Fitzroy",
     address: "abs street",
     tlf: "123456895",
     hours: {
@@ -17,12 +37,83 @@ const storeObj = [
       address: "Bendigo street",
       tlf: "123456895",
       hours: {
-      week: '09:00-17:30',
+      monthurs: '09:00-17:30',
+      fri: '09:00-23:30',
       sat: '11:00-16:30',
       sun: '11:00-12:30',
     },
-    }
+    },
+    {
+        name: "Mildura",
+        address: "Bendigo street",
+        tlf: "123456895",
+        hours: {
+        week: '09:00-17:30',
+        sat: '11:00-16:30',
+        sun: '11:00-12:30',
+      },
+      },
+      {
+        name: "Echuca",
+        address: "Bendigo street",
+        tlf: "123456895",
+        hours: {
+        monweds: '09:00-17:30',
+        thurs: '09:00-22:00',
+        fri: '08:30-20:00',
+        sat: '11:00-16:30',
+        sun: '11:00-12:30',
+      },
+      },    
+      {
+        name: "Bendigo",
+        address: "Bendigo street",
+        tlf: "123456895",
+        hours: {
+        monthurs: '09:00-17:30',
+        fri: '09:00-23:30',
+        sat: '11:00-16:30',
+        sun: '11:00-12:30',
+      },
+      },
+      {
+          name: "Mildura",
+          address: "Bendigo street",
+          tlf: "123456895",
+          hours: {
+          week: '09:00-17:30',
+          sat: '11:00-16:30',
+          sun: '11:00-12:30',
+        },
+        },
+        {
+          name: "Echuca",
+          address: "Bendigo street",
+          tlf: "123456895",
+          hours: {
+          monweds: '09:00-17:30',
+          thurs: '09:00-22:00',
+          fri: '08:30-20:00',
+          sat: '11:00-16:30',
+          sun: '11:00-12:30',
+        },
+    },
+    {
+        name: "Echuca",
+        address: "Bendigo street",
+        tlf: "123456895",
+        hours: {
+        monweds: '09:00-17:30',
+        thurs: '09:00-22:00',
+        fri: '08:30-20:00',
+        sat: '11:00-16:30',
+        sun: '11:00-12:30',
+      },
+  }
    ]
+
+   /* sort objs in array by name */
+   storeObj.sort((a, b) => a.name.localeCompare(b.name));
   
   /* loop to populate page with information stored in array */
   for (let i=0; i < storeObj.length; i++) {
@@ -38,7 +129,7 @@ const storeObj = [
     const title = document.createElement('h2');
     title.classList.add('h2');
     title.classList.add('store-title');
-    title.innerText = "Homewares Galore - " + storeObj[i].name ;
+    title.innerText = storeObj[i].name;
     storeItem.appendChild(title);
     
     /* second indiv container */
@@ -70,7 +161,25 @@ const storeObj = [
     for(var j in openingHours) {
       console.log(j);
       const oh = document.createElement('li');
-      oh.innerText = j + " - " + openingHours[j];
+    
+      var ohText = "";
+      if (j === "week"){
+        ohText = "Monday - Friday:";      
+      } else if (j === "monweds") {
+        ohText = "Monday - Wednesday:";
+      } else if (j === "thurs") {
+        ohText = "Thursday:";
+      } else if (j === "monthurs") {
+        ohText = "Monday - Thursday:";
+      } else if (j === "fri") {
+        ohText = "Friday:";
+      } else if (j === "sat"){
+        ohText = "Saturday:";
+      } else if (j === "sun"){
+        ohText = "Sunday:";
+      }
+      oh.innerHTML = "<span>"+ ohText + "</span>" + openingHours[j];
+      //oh.innerText = ohText + openingHours[j];
       sec.appendChild(oh);
     }
     
